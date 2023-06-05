5fb95a53c1f2ebaa33e318c81736ceba93c6e07e
meppu <meppu@proton.me>
2023-06-05 23:56:31 +0300

    install vsc extensions from `pkgs.vscode-extensions` if possible

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -3,13 +3,14 @@
 {
   programs.vscode = {
     enable = true;
-    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "Nix";
-        publisher = "bbenoist";
-        version = "1.0.1";
-        sha256 = "qwxqOGublQeVP2qrLF94ndX/Be9oZOn+ZMCFX1yyoH0=";
-      }
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      jakebecker.elixir-ls
+      ritwickdey.liveserver
+      rust-lang.rust-analyzer
+      serayuzgur.crates
+      bungcip.better-toml
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
         name = "vscode-zig";
         publisher = "ziglang";
@@ -21,18 +22,6 @@
         publisher = "pgourlain";
         version = "0.9.3";
         sha256 = "9aMkAAx88iJg5R8OKXTyiTZ6N/jJNHM9pBrUA6+e9JE=";
-      }
-      {
-        name = "elixir-ls";
-        publisher = "JakeBecker";
-        version = "0.14.7";
-        sha256 = "RkwgQqasBKMA+0293QhbZhgyGSqhJSic5DuIpBB+OEA=";
-      }
-      {
-        name = "LiveServer";
-        publisher = "ritwickdey";
-        version = "5.7.9";
-        sha256 = "w0CYSEOdltwMFzm5ZhOxSrxqQ1y4+gLfB8L+EFFgzDc=";
       }
       {
         name = "min-theme";
@@ -51,24 +40,6 @@
         publisher = "miguelsolorio";
         version = "0.0.18";
         sha256 = "sE0A441QPwokBoLoCqtImDHmlAXd66fj8zsJR7Ci+Qs=";
-      }
-      {
-        name = "rust-analyzer";
-        publisher = "rust-lang";
-        version = "0.4.1540";
-        sha256 = "prRGT483TgcWP0FSX3Fxw6c15gTP2kFlSJUkO/BvCM0=";
-      }
-      {
-        name = "crates";
-        publisher = "serayuzgur";
-        version = "0.5.11";
-        sha256 = "CuUc68SRJWbnChLJjHqZwPbzptxG85qHJ53IrAZc2zc=";
-      }
-      {
-        name = "better-toml";
-        publisher = "bungcip";
-        version = "0.3.2";
-        sha256 = "g+LfgjAnSuSj/nSmlPdB0t29kqTmegZB5B1cYzP8kCI=";
       }
     ];
   };

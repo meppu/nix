9371039f67b350e5a9b853c0461dadbc39b6d928
meppu <meppu@proton.me>
2023-06-15 01:58:03 +0300

    add auto optimise store

--- system/shared/nix.nix
+++ system/shared/nix.nix
@@ -3,12 +3,15 @@
 {
   nix = {
     gc = {
-	  automatic = true;
-	  dates = "weekly";
-	  options = "--delete-older-than 14d";
-	};
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
 
-    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
   };
   
   nixpkgs.config.allowUnfree = true;

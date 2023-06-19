11dc23e6cece96115ab04eac67e5187ae1bc83e2
meppu <meppu@proton.me>
2023-06-19 21:29:49 +0300

    add some fish functions

--- system/shared/shell.nix
+++ system/shared/shell.nix
@@ -5,6 +5,14 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting
+
+      function flake-env
+        cd $argv && echo "code . && exit" | nix develop --impure
+      end
+
+      function sys-up
+        sudo nixos-rebuild switch --flake $argv --upgrade
+      end
     '';
   };
 }

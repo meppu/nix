b314aa65ca31054b77082acbbf7a25ace997bbab
meppu <git@meppu.boo>
2023-07-08 23:45:53 +0300

    remove steam from desktop config

--- system/profiles/desktop/configuration.nix
+++ system/profiles/desktop/configuration.nix
@@ -14,7 +14,6 @@
     ../../shared/nix.nix
     ../../shared/nvidia.nix
     ../../shared/shell.nix
-    ../../shared/steam.nix
     ../../shared/virtualisation.nix
     ../../shared/sound.nix
   ];

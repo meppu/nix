ac170b3c877517f11aedff19b96ba4082b86bf9c
meppu <meppu@proton.me>
2023-08-08 00:00:42 +0300

    update vscode extensions

--- home/desktop/meppu/vscode/default.nix
+++ home/desktop/meppu/vscode/default.nix
@@ -14,13 +14,20 @@ in
       ritwickdey.liveserver
       rust-lang.rust-analyzer
       serayuzgur.crates
-      bungcip.better-toml
+      tamasfe.even-better-toml
       esbenp.prettier-vscode
       usernamehw.errorlens
       eamodio.gitlens
       yzhang.markdown-all-in-one
       oderwat.indent-rainbow
+      denoland.vscode-deno
     ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "lit-html";
+        publisher = "bierner";
+        version = "1.11.1";
+        sha256 = "sha256-bN786jjTKkcrF0UUOG/J1/k1wqM7JfUO1pQomWLu8+I=";
+      }
       {
         name = "better-comments";
         publisher = "aaron-bond";
@@ -30,8 +37,8 @@ in
       {
         name = "Bookmarks";
         publisher = "alefragnani";
-        version = "13.3.1";
-        sha256 = "sha256-CZSFprI8HMQvc8P9ZH+m0j9J6kqmSJM1/Ik24ghif2A=";
+        version = "13.4.1";
+        sha256 = "sha256-MFr5oePj1gqPXkUi+kvXHhgckw28EAbM48Q3p3oWKCs=";
       } 
       {
         name = "vscode-zig";
@@ -42,8 +49,8 @@ in
       {
         name = "erlang-ls";
         publisher = "erlang-ls";
-        version = "0.0.39";
-        sha256 = "sha256-R0+NvUw+qQRSD8F9L5bS/AqxCMwoPTYYIfb4L+rxduc=";
+        version = "0.0.40";
+        sha256 = "sha256-HFlOig5UUsT+XX0h1dcRQ3mWRsASqvKTMpqqRhVpTAY=";
       }
       {
         name = "min-theme";
@@ -54,8 +61,8 @@ in
       {
         name = "symbols";
         publisher = "miguelsolorio";
-        version = "0.0.12";
-        sha256 = "sha256-eZ/lVoH99ZQA0Xoke941HSuDm6c5JF3owrAvv0uYVK4=";
+        version = "0.0.14";
+        sha256 = "sha256-nCtcTirVoOKa5cuxarqzYXmR7Co0TP+b2XxjAjfOtCo=";
       }
       {
         name = "fluent-icons";
